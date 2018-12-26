d1f7015dc6ce6f76065e44924fa1b316dab29e1f
Will Price <will.price94@gmail.com>
2018-12-26 19:18:14 +0000

    Add missing tests/data/media folder

---
+++ tests/data/media/gen_test_media.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -ex
+
+SRC_VIDEO=big_buck_bunny_360p_5mb.mp4
+
+if [[ ! -f "$SRC_VIDEO" ]]; then
+    wget -c \
+        "https://sample-videos.com/video123/mp4/360/big_buck_bunny_360p_5mb.mp4" \
+        -O "$SRC_VIDEO"
+fi
+
+
+for i in $(seq 0 10); do
+    dest_dir="video_image_folder/video$i"
+    mkdir -p "$dest_dir"
+    if [[ ! -f "$dest_dir/frame_00001.jpg" ]]; then
+        ffmpeg -i "$SRC_VIDEO" -ss "00:00:$(printf %02d $i)" -t 2 "$dest_dir/frame_%05d.jpg"
+    fi
+done
+
+if [[ -d gulp_output ]]; then
+    rm -rf gulp_output
+fi
+
+gulp_20bn_csv_jpeg \
+    --videos_per_chunk 2 \
+    --num_workers 1 \
+    gulp_videos.csv \
+    video_image_folder \
+    gulp_output
+

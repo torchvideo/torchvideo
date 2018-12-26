32424fdf86e021c049db6a66cf3f9b88a9b117ce
Will Price <will.price94@gmail.com>
2018-12-26 20:00:48 +0000

    Remove unused imports from setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 import sys
 from shutil import rmtree
 
-from setuptools import find_packages, setup, Command, find_namespace_packages
+from setuptools import setup, Command, find_namespace_packages
 
 _here = os.path.dirname(__file__)
 print(_here)

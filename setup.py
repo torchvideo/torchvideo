ab8695fb1ae7c134e6329a73397521d137d527fc
Will Price <will.price94@gmail.com>
2019-01-03 10:13:12 +0000

    Ditch python 3.5 support
    conda-forge doesn't support py3.5 anymore, so given that we depend on
    lintel and pyav, installing these without conda is a massive PITA I'm
    giving up on py3.5 support.

--- setup.py
+++ setup.py
@@ -15,25 +15,23 @@ _here = os.path.dirname(__file__)
 print(_here)
 os.system("ls " + _here)
 about = {}
-with open(os.path.join(_here, 'src', 'torchvideo', '__version__.py')) as f:
+with open(os.path.join(_here, "src", "torchvideo", "__version__.py")) as f:
     exec(f.read(), about)
 
 # Package meta-data.
-REQUIRES_PYTHON = '>=3.5.0'
+REQUIRES_PYTHON = ">=3.5.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
     # 'requests', 'maya', 'records',
-    'numpy',
-    'torch',
-    'torchvision',
-    'gulpio'
+    "numpy",
+    "torch",
+    "torchvision",
+    "gulpio",
 ]
 
 # What packages are optional?
-EXTRAS = {
-    'visualisation': ['moviepy'],
-}
+EXTRAS = {"visualisation": ["moviepy"]}
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
@@ -44,22 +42,22 @@ EXTRAS = {
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(_here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
-    long_description = about['__description__']
+    long_description = about["__description__"]
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -69,60 +67,56 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(_here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(_here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
 
         sys.exit()
 
 
 # Where the magic happens:
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__description__'],
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=about['__author__'],
-    author_email=about['__author_email__'],
+    long_description_content_type="text/markdown",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
     python_requires=REQUIRES_PYTHON,
-    url=about['__url__'],
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    url=about["__url__"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MPL2+',
+    license="MPL2+",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand},
 )

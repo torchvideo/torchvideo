3ec1e12632c60fc788a121e795683429feaa820a
Will Price <will.price94@gmail.com>
2019-01-11 16:04:15 +0000

    Put TemporalSegmentSampler tests inside class

--- tests/unit/samplers/test_temporal_segment_sampler.py
+++ tests/unit/samplers/test_temporal_segment_sampler.py
@@ -7,72 +7,66 @@ from assertions.seq import assert_ordered
 from torchvideo.samplers import TemporalSegmentSampler
 
 
-def test_segment_sampler_with_fewer_frames_than_segments():
-    sampler = TemporalSegmentSampler(3, 1)
-    frame_count = 2
+class TestTemporalSegmentSampler:
+    def test_fewer_frames_than_segments(self):
+        sampler = TemporalSegmentSampler(3, 1)
+        frame_count = 2
 
-    frame_idx = sampler.sample(frame_count)
+        frame_idx = sampler.sample(frame_count)
 
-    assert frame_idx == [0, 0, 0]
+        assert frame_idx == [0, 0, 0]
 
+    def test_average_segment_frame_count_less_than_1(self):
+        segment_count = 3
+        sampler = TemporalSegmentSampler(segment_count, 1)
+        frame_count = 4
 
-def test_segment_sampler_with_average_segment_frame_count_less_than_1():
-    segment_count = 3
-    sampler = TemporalSegmentSampler(segment_count, 1)
-    frame_count = 4
+        frame_idx = sampler.sample(frame_count)
 
-    frame_idx = sampler.sample(frame_count)
+        assert len(frame_idx) == segment_count
+        assert_ordered(frame_idx)
+        assert np.all(np.array(frame_idx) < frame_count)
 
-    assert len(frame_idx) == segment_count
-    assert_ordered(frame_idx)
-    assert np.all(np.array(frame_idx) < frame_count)
+    def test_average_segment_frame_count_greater_than_1(self):
+        segment_count = 3
+        sampler = TemporalSegmentSampler(segment_count, 1)
+        frame_count = 6
 
+        frame_idx = sampler.sample(frame_count)
 
-def test_segment_sampler_with_average_segment_frame_count_greater_than_1():
-    segment_count = 3
-    sampler = TemporalSegmentSampler(segment_count, 1)
-    frame_count = 6
+        assert len(frame_idx) == segment_count
+        assert (
+            len(set(frame_idx)) == segment_count
+        )  # segment indices should be unique if possible
+        assert_ordered(frame_idx)
+        assert np.all(np.array(frame_idx) < frame_count)
 
-    frame_idx = sampler.sample(frame_count)
+    @given(st.integers(1, 100), st.integers(1, 1000), st.integers(1, 10000))
+    def test_index_properties(self, segment_count, segment_length, video_length):
+        sampler = TemporalSegmentSampler(segment_count, segment_length)
 
-    assert len(frame_idx) == segment_count
-    assert (
-        len(set(frame_idx)) == segment_count
-    )  # segment indices should be unique if possible
-    assert_ordered(frame_idx)
-    assert np.all(np.array(frame_idx) < frame_count)
+        frame_idx = sampler.sample(video_length)
 
+        assert len(frame_idx) == segment_count
+        assert_ordered(frame_idx)
+        assert np.all(np.array(frame_idx) < video_length)
 
-@given(st.integers(1, 100), st.integers(1, 1000), st.integers(1, 10000))
-def test_segment_sampler_index_properties(segment_count, segment_length, video_length):
-    sampler = TemporalSegmentSampler(segment_count, segment_length)
+    def test_repr(self):
+        assert (
+            repr(TemporalSegmentSampler(1, 5))
+            == "TemporalSegmentSampler(segment_count=1, segment_length=5)"
+        )
 
-    frame_idx = sampler.sample(video_length)
+    def test_str(self):
+        assert (
+            str(TemporalSegmentSampler(1, 5))
+            == "TemporalSegmentSampler(segment_count=1, segment_length=5)"
+        )
 
-    assert len(frame_idx) == segment_count
-    assert_ordered(frame_idx)
-    assert np.all(np.array(frame_idx) < video_length)
+    def test_segment_length_should_be_greater_than_0(self):
+        with pytest.raises(ValueError):
+            TemporalSegmentSampler(1, 0)
 
-
-def test_segment_sampler_repr():
-    assert (
-        repr(TemporalSegmentSampler(1, 5))
-        == "TemporalSegmentSampler(segment_count=1, segment_length=5)"
-    )
-
-
-def test_segment_sampler_str():
-    assert (
-        str(TemporalSegmentSampler(1, 5))
-        == "TemporalSegmentSampler(segment_count=1, segment_length=5)"
-    )
-
-
-def test_segment_length_should_be_greater_than_0():
-    with pytest.raises(ValueError):
-        TemporalSegmentSampler(1, 0)
-
-
-def test_segment_count_should_be_greater_than_0():
-    with pytest.raises(ValueError):
-        TemporalSegmentSampler(0, 1)
+    def test_segment_count_should_be_greater_than_0(self):
+        with pytest.raises(ValueError):
+            TemporalSegmentSampler(0, 1)

800175f45d7d80f0016bd18a3942d6bb75be6541
Will Price <will.price94@gmail.com>
2019-01-13 17:38:50 +0000

    Remove dead set_video_backend code

--- src/torchvideo/__init__.py
+++ src/torchvideo/__init__.py
@@ -1,51 +1,10 @@
-import warnings
-
 from . import datasets, transforms, samplers
 from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import __author__, __author_email__, __license__, __copyright__
 
-__all__ = [
-    "set_video_backend",
-    "get_video_backend",
-    "datasets",
-    "transforms",
-    "samplers",
-]
+__all__ = ["get_video_backend", "datasets", "transforms", "samplers"]
 
-_default_backend = "lintel"
-_video_backend = _default_backend
-
-
-def set_video_backend(backend: str) -> None:
-    global _video_backend
-    backend = backend.lower()
-    if backend not in {"lintel", "nvvl"}:
-        raise ValueError(
-            (
-                "Invalid video backend '{}'. "
-                "Options are: 'lintel', 'pyav', and 'nvvl'"
-            ).format(backend)
-        )
-    if backend == "pyav":
-        try:
-            import av
-        except ImportError:
-            warnings.warn(
-                "PyAV not installed, defaulting to {}".format(_default_backend),
-                RuntimeWarning,
-            )
-            backend = _default_backend
-    if backend == "nvvl":
-        try:
-            import nvvl
-        except ImportError:
-            warnings.warn(
-                "nvvl not installed, defaulting to {}".format(_default_backend),
-                RuntimeWarning,
-            )
-            backend = _default_backend
-
-    _video_backend = backend
+_video_backend = "lintel"
 
 
 def get_video_backend() -> str:

b94d4c4b18e630daca0bb5ea2400e7b6e4672809
Will Price <will.price94@gmail.com>
2019-01-13 16:00:58 +0000

    Improve sampler tests and ensure they always return valid idx

--- tests/unit/samplers/__init__.py
+++ tests/unit/samplers/__init__.py
@@ -1,8 +1,1 @@
 from assertions.seq import assert_ordered, assert_elems_lt, assert_elems_gte
-
-
-def assert_valid_frame_index(frame_idx, expected_frame_count, video_length):
-    assert len(frame_idx) == expected_frame_count
-    assert_ordered(frame_idx)
-    assert_elems_lt(frame_idx, video_length)
-    assert_elems_gte(frame_idx, 0)
--- tests/unit/test_samplers.py
+++ tests/unit/test_samplers.py
@@ -1,44 +1,106 @@
-from random import randint
-from typing import List
+# PyTest has weird syntax for parameterizing fixtures:
+# https://docs.pytest.org/en/latest/fixture.html#parametrizing-fixtures
 
 import pytest
 from hypothesis import given
 import hypothesis.strategies as st
 
+from assertions.seq import assert_ordered, assert_elems_gte, assert_elems_lt
 from torchvideo.internal.utils import frame_idx_to_list
-from torchvideo.samplers import TemporalSegmentSampler
-from unit.samplers import assert_valid_frame_index
+from torchvideo.samplers import (
+    FullVideoSampler,
+    TemporalSegmentSampler,
+    ClipSampler,
+    LambdaSampler,
+)
 
 
-class TS:
-    def __init__(self, segment_count, snippet_length):
-        self.segment_count = segment_count
-        self.snippet_length = snippet_length
+def full_video_sampler():
+    return FullVideoSampler()
 
-    def sample(self, video_length):
-        segment_length = video_length // self.segment_count
-        segment_start_idx = self.segment_start_idx(video_length, segment_length)
-        offsets = [
-            self.random_snippet_offset(segment_length)
-            for _ in range(self.segment_count)
-        ]
-        idx = [
-            [segment_start + offset + i]
-            for i in range(self.snippet_length)
-            for segment_start, offset in zip(segment_start_idx, offsets)
-        ]
 
-    def segment_start_idx(self, video_length: int, segment_length: int) -> List[int]:
-        return [segment_length * i for i in range(self.segment_count)]
+def temporal_segment_sampler():
+    segment_count = st.integers(1, 100).example()
+    snippet_length = st.integers(1, 1000).example()
+    return TemporalSegmentSampler(segment_count, snippet_length)
 
-    def random_snippet_offset(self, segment_length):
-        max_offset = segment_length - self.snippet_length
-        if max_offset == 0:
-            return max_offset
-        return randint(max_offset)
 
-    def central_snippet_offset(self, segment_length):
-        return (segment_length - self.snippet_length) // 2
+def clip_sampler():
+    clip_length = st.integers(1, 1000).example()
+    return ClipSampler(clip_length)
+
+
+@pytest.fixture(params=[clip_sampler, full_video_sampler, temporal_segment_sampler])
+def frame_sampler(request):
+    return request.param()
+
+
+class TestFrameSampler:
+    """
+    These tests test general properties that all samplers should abide by.
+    """
+
+    @given(st.integers(max_value=0))
+    def test_frame_sampler_raises_error_0_or_negative_frame_count(
+        self, frame_sampler, frame_count
+    ):
+        with pytest.raises(ValueError, match=r"{} frames".format(frame_count)):
+            frame_sampler.sample(frame_count)
+
+    @given(st.integers(min_value=1, max_value=1000))
+    def test_frame_sampler_generates_sequential_idx(self, frame_sampler, frame_count):
+        frames_idx = frame_sampler.sample(frame_count)
+        frames_idx = frame_idx_to_list(frames_idx)
+
+        assert_ordered(frames_idx)
+        assert_elems_lt(frames_idx, frame_count)
+        assert_elems_gte(frames_idx, 0)
+
+
+class TestFullVideoSampler:
+    @given(st.integers(min_value=1))
+    def test_full_video_sampler(self, length):
+        sampler = FullVideoSampler()
+
+        idx_slice = sampler.sample(length)
+
+        assert idx_slice.stop == length
+        assert idx_slice.start == 0
+        assert idx_slice.step == 1
+
+    def test_full_video_sampler_repr(self):
+        assert repr(FullVideoSampler()) == "FullVideoSampler()"
+
+    def test_full_video_sampler_str(self):
+        assert str(FullVideoSampler()) == "FullVideoSampler()"
+
+
+class TestClipSampler:
+    @given(st.data())
+    def test_clip_is_subsampled_from_video_when_video_is_longer_than_clip(self, data):
+        clip_length = data.draw(st.integers(1, 1000))
+        video_length = data.draw(st.integers(clip_length, 1000))
+        sampler = ClipSampler(clip_length=clip_length)
+
+        frame_idx = sampler.sample(video_length)
+
+        assert frame_idx.step == 1
+        assert (frame_idx.stop - frame_idx.start) == clip_length
+
+    @given(st.data())
+    def test_clip_is_oversampled_when_video_is_shorter_than_clip_length(self, data):
+        clip_length = data.draw(st.integers(2, 1000))
+        video_length = data.draw(st.integers(1, clip_length - 1))
+        sampler = ClipSampler(clip_length=clip_length)
+
+        frame_idx = sampler.sample(video_length)
+        frame_idx = frame_idx_to_list(frame_idx)
+
+        assert_elems_lt(frame_idx, clip_length - 1)
+        assert_elems_gte(frame_idx, 0)
+
+    def test_repr(self):
+        assert repr(ClipSampler(10)) == "ClipSampler(clip_length=10, frame_step=1)"
 
 
 class TestTemporalSegmentSampler:
@@ -91,6 +153,7 @@ class TestTemporalSegmentSampler:
         for i in range(1000):
             frame_idx = self.sample(100, 2, 1)
             seen_idx.add(tuple(frame_idx))
+
         # A bit handwavy, but basically we want to ensure have a reasonable number of
         # segment positions. We generate 1000 frame_idx, for 2 snippets of length 1.
         assert len(seen_idx) > 10
@@ -112,7 +175,9 @@ class TestTemporalSegmentSampler:
     @given(st.data())
     def test_fuzz_sampler_test(self, data):
         segment_count, snippet_length, video_length = self.draw_sampler_parameters(data)
+
         frame_idx = self.sample(video_length, segment_count, snippet_length, test=True)
+
         assert_valid_frame_index(
             frame_idx, segment_count * snippet_length, video_length
         )
@@ -148,3 +213,30 @@ class TestTemporalSegmentSampler:
         snippet_length = data.draw(st.integers(1, 100))
         video_length = data.draw(st.integers(1, 10000))
         return segment_count, snippet_length, video_length
+
+
+class TestLambdaSampler:
+    def test_throws_error_if_user_provided_sampling_fn_returns_invalid_idx(self):
+        sampler = LambdaSampler(lambda frames: [frames + 1])
+
+        with pytest.raises(ValueError):
+            sampler.sample(10)
+
+    def test_repr(self):
+        class MySampler:
+            def __call__(self, video_length):
+                return slice(0, video_length, 1)
+
+            def __repr__(self):
+                return self.__class__.__name__ + "()"
+
+        sampler = LambdaSampler(MySampler())
+
+        assert repr(sampler) == "LambdaSampler(sampler=MySampler())"
+
+
+def assert_valid_frame_index(frame_idx, expected_frame_count, video_length):
+    assert len(frame_idx) == expected_frame_count
+    assert_ordered(frame_idx)
+    assert_elems_lt(frame_idx, video_length)
+    assert_elems_gte(frame_idx, 0)

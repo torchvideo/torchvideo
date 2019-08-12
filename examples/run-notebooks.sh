c812c5f6a42bf7da4e83d8e6694e18184fc10d11
Will Price <will.price94@gmail.com>
2019-08-12 09:14:56 +0100

    Support longer running cells in nbs

--- examples/run-notebooks.sh
+++ examples/run-notebooks.sh
@@ -3,5 +3,7 @@ NOTEBOOK_DIR="$(dirname $(readlink -f $0))"
 set -ex
 
 for f in "$NOTEBOOK_DIR"/*.ipynb; do
-    jupyter nbconvert --to notebook --execute --inplace "$f"
+    # Some cells can take a long time to execute, by default jupyter
+    # only has a 30s timeout, so we increase this
+    jupyter nbconvert --ExecutePreprocessor.timeout=120 --to notebook --execute --inplace "$f"
 done

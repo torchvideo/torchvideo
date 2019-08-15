24e6d3295684388f04247537fe3a6269f1d219c3
Will Price <will.price94@gmail.com>
2019-08-15 17:14:21 +0100

    Implement #52: Support channel_dim in NormalizeVideo and normalize (#54)
    Closes #52

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,22 @@
 from pathlib import Path
 
+import hypothesis
+import numpy as np
+import torch
+
+
+class TorchRandomState:
+    def getstate(self) -> np.ndarray:
+        return torch.get_rng_state().numpy()
+
+    def setstate(self, state: np.ndarray) -> None:
+        return torch.set_rng_state(torch.from_numpy(state))
+
+    def seed(self, seed) -> None:
+        torch.manual_seed(seed)
+
+
+hypothesis.register_random(TorchRandomState())
+
 _here = Path(__file__).parent
 TEST_DATA_ROOT = _here / "data"

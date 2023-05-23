ff9391c2575def566900d2d7fa15ab3bf2d4d4b1
Amir Rosenfeld <amir@theator.io>
2023-05-23 11:41:48 +0300

    Create flip_color_channels_video.py

---
+++ src/torchvideo/transforms/transforms/flip_color_channels_video.py
@@ -0,0 +1,25 @@
+from typing import Iterator, Union, Tuple
+
+from PIL.Image import Image
+from torchvision.transforms import transforms as tv
+
+from .types import PILVideo
+from .transform import Transform
+import numpy as np
+
+
+class FlipColorChannelsVideo(Transform[PILVideo, Iterator[Image], None]):
+    """Flips the colors (rgb->bgr) of given video (composed of PIL Images).
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        # self._image_transform = tv.CenterCrop(size)
+
+    def _gen_params(self, frames):
+        return None
+
+    def _transform(self, frames: PILVideo, params):
+        for frame in frames:
+            yield Image.fromarray(np.array(frame)[:, :, ::-1])
